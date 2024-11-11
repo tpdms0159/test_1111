693e2435c4d9ef49991421c6796004c0680d2c4e
tpdms <tpdms0159@naver.com>
2024-11-11 09:54:56 +0900

    init

---
+++ src/main/java/naver/tpdms0159/test_1111/Test1111Application.java
@@ -0,0 +1,13 @@
+package naver.tpdms0159.test_1111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test1111Application.class, args);
+    }
+
+}
---
+++ src/test/java/naver/tpdms0159/test_1111/Test1111ApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.tpdms0159.test_1111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
